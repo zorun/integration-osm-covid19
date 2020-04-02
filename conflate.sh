43b00ff70bd63921851804407190b99e0e9a9d48
Baptiste Jonglez <git@bitsofnetworks.org>
2020-04-02 11:20:19 +0200

    Change output filename

--- conflate.sh
+++ conflate.sh
@@ -7,9 +7,9 @@ DATE="20200331"
 while read city
 do
     data="data-in/dokomaps_covid19_${DATE}_licence-odbl-${city}.csv"
-    matchfile="data-out/dokomaps-match-${DATE}-${city}.csv"
-    changefile="data-out/dokomaps-change-${DATE}-${city}.json"
-    osmfile="data-out/dokomaps-osm-${DATE}-${city}.osm"
+    matchfile="data-out/dokomaps-${DATE}-${city}.csv"
+    changefile="data-out/dokomaps-${DATE}-${city}.json"
+    osmfile="data-out/dokomaps-${DATE}-${city}.osm"
     printf "\nNow conflating $city\n"
     conflate dokomaps_profile.py -i "$data" -l "$matchfile" -o "$osmfile" -c "$changefile"
     # Overpass API a du rate-limiting
